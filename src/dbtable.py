90d18840a831e95bcebcea6ce367bb0e9c9023d4
st0rmg0d <55540414+st0rmg0d@users.noreply.github.com>
2021-10-28 00:54:53 +0600

    Update dbtable.py

--- src/dbtable.py
+++ src/dbtable.py
@@ -4,14 +4,11 @@ from sqlalchemy.dialects import postgresql
 from sqlalchemy.sql import text
 from sqlalchemy import create_engine
 
-
 app = Flask(__name__)     
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:st0rmg0d@localhost/webserver'  
 db = SQLAlchemy(app)  
 
-
 engine = create_engine('postgresql://postgres:st0rmg0d@localhost/webserver') 
-
 
 class user(db.Model):          
     __tablename__ = 'usser'
@@ -20,13 +17,11 @@ class user(db.Model):
     password = db.Column('password', db.Unicode)
     token = db.Column('token', db.Unicode)
 
-
     def __init__(self,usserid,login,password, token):  
         self.usserid = usserid
         self.login = login
         self.password = password
         self.token = token
-
 
     login = ''     
     password = ''  
